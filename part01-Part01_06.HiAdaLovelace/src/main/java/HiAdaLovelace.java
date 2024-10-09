61471313914e21dbe238e9b3c5c318ce642d0c7a
tomás <67541868+tomasproanop@users.noreply.github.com>
2024-10-09 22:02:05 +0200

    Add files via upload

---
+++ part01-Part01_06.HiAdaLovelace/src/main/java/HiAdaLovelace.java
@@ -0,0 +1,10 @@
+
+public class HiAdaLovelace {
+
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+        String greeting = "Hi";
+        String exc = "!";
+        System.out.println(greeting + " " + name + exc);
+    }
+}
